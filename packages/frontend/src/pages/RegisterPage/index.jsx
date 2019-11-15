4d4cdcb3411b3e4d4b02e990f92f24904b3df2ff
Amire <amohm001@plattsburgh.edu>
2019-11-15 01:10:29 -0500

    added the picture on the right side of the register page

--- packages/frontend/src/pages/RegisterPage/index.jsx
+++ packages/frontend/src/pages/RegisterPage/index.jsx
@@ -3,12 +3,14 @@ import "./styles.css"
 import { Title } from "../../components/Title"
 import { InputBlock } from "../../components/InputBlock";
 import Logo from "../../assets/logo.svg"
+import Pab from "../../assets/pabloNavi.svg"
 
 export class RegisterPage extends React.Component {
     render() {
         return (
             <div style={{ margin: "20px" }}>
                 <img style={{ textAlign: "left" }} src={Logo} alt=""></img>
+
                 <h1>
                     <h5 style={{ textAlign: "justify", color: "#ff5a6e", fontSize: "40px" }}>REGISTER TO NAVICOURSE</h5>
                     <h3 style={{ textAlign: "justify", fontFamily: "Times Roman", fontSize: "20px", padding: "15px", top: "100px", color: "Black" }}>Welcome! Please register to review and read people’s reviews</h3>
@@ -20,6 +22,12 @@ export class RegisterPage extends React.Component {
                 </div>
                 <button style={{ textAlign: "center", backgroundColor: "#ff5a6e", color: "white", width: "150px", height: "50px", borderRadius: "12%", fontSize: "20px", padding: "0px", marginLeft: "59px", marginTop: "60px" }}>Register</button>
                 <h3 style={{ textAlign: "justify", fontFamily: "Times Roman", fontSize: "20px", padding: "25px", color: "Black" }}>Already have an account? Sign in</h3>
+                <div className="container">
+                    <div className="login-landing">
+                        <img src={Pab} alt="Man"></img>
+                    </div>
+                </div>
+
             </div>
         );
     }
