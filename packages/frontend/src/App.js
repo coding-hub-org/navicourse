1ba6a9dcef0b518ac279d1b0ffbf395c83c1ba6d
Amire <amohm001@plattsburgh.edu>
2019-11-29 22:09:54 -0500

    Switch for register

--- packages/frontend/src/App.js
+++ packages/frontend/src/App.js
@@ -1,22 +1,23 @@
 import React from 'react'
 
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import { AuthButton } from "./components/Button";
 import { InputBlock } from "./components/InputBlock";
 import { Title } from "./components/Title";
 import { LoginPage } from "./pages/LoginPage"
+import { RegisterPage } from "./pages/RegisterPage"
 
 
 const App = () => {
-	return (
+  return (
     <Router>
       <Switch>
-        <Route exact path="/login" component={LoginPage} />
-        <Route component={LoginPage} />
+        <Route exact path="/register" component={RegisterPage} />
+        <Route component={RegisterPage} />
       </Switch>
     </Router>
-	);
+  );
 }
 
 
